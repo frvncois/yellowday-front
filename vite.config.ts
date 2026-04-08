6385cc300750da58e0db931f4ac1ff5a8ec95779
Frvncois <hello@frvncois.com>
2026-04-08 13:11:20 -0400

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     tailwindcss(),
     vue(),
-    vueDevTools(),
+    mode === 'development' && vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
